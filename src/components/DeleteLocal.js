dbea44ecf59eb9c6c79d929add3ca681e8f6eda4
Renan Ribeiro <renan.lagee@gmail.com>
2023-03-26 16:00:13 -0300

    Delete local refatorado

--- src/components/DeleteLocal.js
+++ src/components/DeleteLocal.js
@@ -1,42 +1,44 @@
 import {
   Box,
-  Button, IconButton,
-  Modal, Typography
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import React from 'react';
+  Button,
+  IconButton,
+  Modal,
+  Typography
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   modalContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '70%',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "70%",
   },
   modalHeader: {
     backgroundColor: theme.palette.error.main,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     padding: theme.spacing(1),
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   closeButton: {
-    backgroundColor: 'transparent',
-    color: 'white',
-    fontSize: '1.2rem',
-    fontWeight: 'bold',
+    backgroundColor: "transparent",
+    color: "white",
+    fontSize: "1.2rem",
+    fontWeight: "bold",
     padding: 0,
-    '&:hover': {
-      backgroundColor: 'transparent',
+    "&:hover": {
+      backgroundColor: "transparent",
       color: theme.palette.primary.dark,
     },
   },
   deleteButton: {
     backgroundColor: theme.palette.error.main,
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontWeight: 600,
-    '&:hover': {
+    "&:hover": {
       backgroundColor: theme.palette.error.dark,
     },
   },
@@ -45,33 +47,46 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const DeleteLocalModal = ({ open, onClose, onDelete, locationName, locationId }) => {
+const DeleteLocalModal = ({
+  open,
+  onClose,
+  onDelete,
+  locationName,
+  locationId,
+}) => {
   const classes = useStyles();
 
   return (
     <Modal open={open} onClose={onClose}>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100%"
-      >
+      <Box display="flex" justifyContent="center" alignItems="center" height="100%">
         <Box bgcolor="white" borderRadius={5} maxWidth={800} maxHeight={400} width="100%">
           <Box className={classes.modalHeader}>
             <Typography variant="h4">Confirmação de exclusão</Typography>
-            <IconButton className={classes.closeButton} onClick={onClose} aria-label="Fechar">
+            <IconButton
+              className={classes.closeButton}
+              onClick={onClose}
+              aria-label="Fechar"
+            >
               &times;
             </IconButton>
           </Box>
-        <Box p={2} display="flex" flexDirection="column" minHeight={200} justifyContent="space-between">
+          <Box
+            p={2}
+            display="flex"
+            flexDirection="column"
+            minHeight={200}
+            justifyContent="space-between"
+          >
             <Typography variant="h5" p={3}>
-              O local <span className={classes.locationName}>{locationName}</span> será excluída. Tem certeza dessa ação?
+              O local{" "}
+              <span className={classes.locationName}>{locationName}</span> será
+              excluído. Tem certeza dessa ação?
             </Typography>
             <Box display="flex" justifyContent="flex-end" marginTop="1em">
               <Button
                 variant="contained"
                 className={classes.deleteButton}
-                onClick={()=> onDelete(locationId, onClose)}
+                onClick={() => onDelete(locationId, onClose)}
               >
                 Excluir
               </Button>
